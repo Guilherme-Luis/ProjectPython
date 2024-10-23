5f9d06b8ef414db3bb19157c52d3c0600c3e8d39
Guilherme-Luis <guilherme_silva_luis@hotmail.com>
2024-10-22 21:42:43 -0300

    Quinta atividade feita em lógica

---
+++ Ex5/Exercise05.py
@@ -0,0 +1,18 @@
+"""
+1. Ligar Interruptor A por 10 a 15 minutos
+2. Desligar Interruptor A
+3. Ligar Interruptor B
+4. Ir até a sala das lâmpadas
+
+5. Verificar o estado das lâmpadas:
+   Se Lâmpada A está ACESA:
+       Interruptor B controla Lâmpada A
+   Caso contrário:
+       Se Lâmpada A está APAGADA e QUENTE:
+           Interruptor A controla Lâmpada A
+        Caso contrário:
+           Interruptor C controla Lâmpada A
+       Fim Se
+   Fim Se
+Fim
+"""
